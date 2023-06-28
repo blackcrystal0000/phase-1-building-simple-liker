7898b7119f03a08a564fef7649f891ac8fb15f30
Crystal <muthonigachanja2000@gmail.com>
2023-06-28 08:36:19 -0700

    Completed lab

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal');
 
+modal.className = "hidden";
+
+const buttons = document.getElementsByClassName('like-glyph');
+
+for(let button of buttons){
+  button.addEventListener('click', event => {
+    let button = event.target;
+    event.preventDefault();
+    mimicServerCall()
+    .then(data => {
+      button.className = "activated-heart";
+      console.log(data)
+    })
+    .catch(() => {
+      button.className = ""
+      modal.className = ""
+      setTimeout(function(){
+        modal.className = "hidden"
+      }, 3000);
+      console.log("ERROR");
+    })
+  })
+};
 
 
 
